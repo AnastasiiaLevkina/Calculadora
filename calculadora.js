b519d9c2cc93b6433a0ec324fdb4a41c770828ab
MauroLrodri <162575122+gitMl31@users.noreply.github.com>
2024-06-25 12:48:46 +0200

    Calculadora 1.1
    Cambios de hoy

--- calculadora.js
+++ calculadora.js
@@ -1,294 +1,263 @@
-//test
-let test = not 
-let num1 = 0
-let num2 = 0
+let num1 = 0;
+let num2 = 0;
 let operator = "";
 let result;
-let num1saved = false
-let hasResult = false
+let num1saved = false;
+let hasResult = false;
 
-function inputNumber(num){
+addNumberInputDOMButtonEventListener();
+addClearInputDOMButtonEventListener();
+addDeleteInputDOMButtonEventListener();
+addOperatorInputDOMButtonEventListener();
+addEqualInputDOMButtonEventListener();
 
-    const header = document.querySelector("header");
-
-    if(num1saved){
-        header.textContent = "0"
-        num1saved = false
+function inputNumber(num) {
+  const display = document.querySelector("header");
+  if (num1saved) {
+    display.textContent = "0";
+    num1saved = false;
+  }
+  if (num === ",") {
+    disableComma();
+  }
+  if (num === "," && display.textContent === "0") {
+    display.textContent = "0,";
+  } else {
+    if (display.textContent === "0" || hasResult) {
+      display.textContent = num;
+      hasResult = false;
+    } else if (display.textContent === "-0") {
+      if (num == ",") {
+        display.textContent = "-0,";
+      } else {
+        display.textContent = "-" + num;
+      }
+    } else if (display.textContent.length < 9) {
+      display.textContent = display.textContent + num;
     }
-    
-    if(num === ","){
+  }
 
-        disableComma()
-
-    }
-
-    if(num === "," && header.textContent === "0"){
-
-        header.textContent = "0,"
-
-    } else  {
-
-        if(header.textContent === "0" || hasResult){
-
-            header.textContent = num
-            hasResult = false
-    
-        }else if(header.textContent === "-0"){
-
-            if(num == ","){
-
-                header.textContent = "-0,"
-
-            }else{
-
-                header.textContent = "-" + num
-
-            }
-
-        } else if (header.textContent.length < 9){
-    
-            header.textContent = header.textContent + num;  
-    
-        }
-
-    }
-
-    if(num != ","){
-        const disabledButtons = document.querySelectorAll("button.inactive")
-        disabledButtons.forEach(button => {
-
-            button.classList.remove("inactive")
-            button.removeAttribute("disabled")
-
-        })
-    }
-
+  if (num != ",") {
+    const disabledButtons = document.querySelectorAll("button.inactive");
+    disabledButtons.forEach((button) => {
+      button.classList.remove("inactive");
+      button.removeAttribute("disabled");
+    });
+  }
 }
 
-function disableComma(){
-    
-    const comma = document.getElementById("comma")
-
-    comma.classList.add("inactive")    
-
-    comma.setAttribute("disabled", "")
-
+function disableComma() {
+  const comma = document.getElementById("comma");
+  comma.classList.add("inactive");
+  comma.setAttribute("disabled", "");
 }
 
-function enableComma(){
-    
-    const comma = document.getElementById("comma")
-
-    comma.classList.remove("inactive")    
-
-    comma.removeAttribute("disabled")
-
+function enableComma() {
+  const comma = document.getElementById("comma");
+  comma.classList.remove("inactive");
+  comma.removeAttribute("disabled");
 }
 
-function inputOp(op){
+function inputOp(op) {
+  const display = document.querySelector("header");
+  if (op.textContent == "-") {
+    handleMinusButton(op);
+  } else {
+    desactivateOp();
+    removeHighlight();
+    activateOp(op);
+    operator = op.textContent;
+    num1 = parseFloat(display.textContent.replace(",", "."));
+    num1saved = true;
+    enableComma();
+  }
+}
 
-    const header = document.querySelector("header");
-
-    if(header.textContent == "0" && op.textContent == "-"){
-
-        op.classList.add("active-plus")
-        header.textContent = "-0"
-
-    }else if(op.textContent == "-" && operator.length == 0){
-
-        activateOp(op)
-        operator = op.textContent;
-        num1 = parseFloat(header.textContent.replace(",", "."))
-        num1saved = true
-        enableComma()
-
-    }else if(op.textContent != "-"){
-
-        desactivateOp()
-        activateOp(op)
-        operator = op.textContent;
-        num1 = parseFloat(header.textContent.replace(",", "."))
-        num1saved = true
-        enableComma()
-
+function handleMinusButton(op) {
+  const display = document.querySelector("header");
+  if (display.textContent == "0" || display.textContent == "-0") {
+    changeSign();
+    if (op.classList.contains("active")) {
+      op.classList.remove("active");
+    } else {
+      op.classList.add("active");
     }
-
-}
-
-function activateOp(op){
-
-    op.classList.add("active")
-
-}
-
-function desactivateOp(){
-
-    if(operator && operator.length != 0){
-        const op = document.getElementById("op"+operator)
-        op.classList.remove("active")
+  } else if (operator == "") {
+    desactivateOp();
+    operator = op.textContent;
+    num1 = parseFloat(display.textContent.replace(",", "."));
+    num1saved = true;
+    enableComma();
+  } else if (num1saved && display.textContent != String(num1)) {
+    changeSign();
+    if (op.classList.contains("active-plus")) {
+      removeHighlight();
+    } else {
+      highlightNegative();
     }
-
+  }
 }
 
-function clear(){
-
-    const header = document.querySelector("header");
-    const opButtons = document.querySelectorAll(".op")
-
-    opButtons.forEach(opButton => {
-        opButton.classList.remove("active")
-        opButton.classList.remove("active-plus")
-    })
-
-    num1 = 0
-    num2 = 0
-    operator = ""
-    header.textContent = "0"
-    enableComma()
-    num1saved = false
-    result = 0
-
-    const disabledButtons = document.querySelectorAll("button[name=disable]")
-    disabledButtons.forEach(button => {
-
-        button.classList.add("inactive")
-        button.setAttribute("disabled", "")
-        
-    })
+function removeHighlight() {
+  const op = document.getElementById("op-");
+  op.classList.remove("active-plus");
 }
 
-function del(){
-
-    const header = document.querySelector("header");
-
-    if (header.textContent != "0"){
-
-        if(header.textContent[header.textContent.length - 1] == ","){
-
-            enableComma();
-
-        }
-
-        header.textContent = header.textContent.slice(0,header.textContent.length - 1)
-
-        if(header.textContent.length === 0){
-
-             header.textContent = "0"
-            enableComma()
-        }
-
-    }
-
+function highlightNegative() {
+  const op = document.getElementById("op-");
+  op.classList.add("active-plus");
 }
 
-function equal(){
-    const header = document.querySelector("header");
-    num2 = parseFloat(header.textContent.replace(",", "."))
-
-    switch (operator) {
-        case "+":
-            result = num1 + num2
-            break;
-        case "-":
-            result = num1 - num2
-            break;
-        case "x":
-            result = num1 * num2
-            break;
-        case "/":
-            if(num2 == 0){
-                result = "ERROR"
-            }else{
-                result = num1 / num2
-            }
-            break;
-        default:
-            result = header.textContent
-            break;
-    }
-
-    let formattedResult = String(result)
-
-    if(formattedResult.length > 9){
-
-        if(result > 999999999){
-
-            formattedResult = String(result.toExponential(2))
-
-        }else{
-
-            formattedResult = formattedResult.slice(0 , 8)
-
-        }
-
-    }
-
-    if(num1saved){
-
-        header.textContent = num1
-        num1saved = false
-
-    }else{
-
-        header.textContent = formattedResult.replace(".", ",")
-
-    }
-
-    const opButtons = document.querySelectorAll(".op")
-
-    opButtons.forEach(opButton => {
-        opButton.classList.remove("active")
-        opButton.classList.remove("active-plus")
-    })
-
-    hasResult = true
-    operator = ""
+function changeSign() {
+  const display = document.querySelector("header");
+  if (display.textContent[0] === "-") {
+    display.textContent = display.textContent.slice(
+      1,
+      display.textContent.length
+    );
+  } else {
+    display.textContent = "-" + display.textContent;
+  }
 }
 
-const buttons = document.querySelectorAll("button.num");
+function activateOp(op) {
+  op.classList.add("active");
+}
 
-buttons.forEach(button => {
-    button.addEventListener("click",(event)=> {
+function desactivateOp() {
+  if (operator && operator.length != 0) {
+    const op = document.getElementById("op" + operator);
+    if (operator != "-") {
+      op.classList.remove("active");
+    }
+  }
+}
 
-        let clickedNum = event.target.textContent
+function onClearInput() {
+  const display = document.querySelector("header");
+  const opButtons = document.querySelectorAll(".op");
+  opButtons.forEach((opButton) => {
+    opButton.classList.remove("active");
+    opButton.classList.remove("active-plus");
+  });
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+  display.textContent = "0";
+  enableComma();
+  num1saved = false;
+  result = 0;
+  const disabledButtons = document.querySelectorAll("button[name=disable]");
+  disabledButtons.forEach((button) => {
+    button.classList.add("inactive");
+    button.setAttribute("disabled", "");
+  });
+}
 
-        inputNumber(clickedNum)
-        console.log(clickedNum);
-    })
-})
+function onDeleteInput() {
+  const display = document.querySelector("header");
+  if (display.textContent != "0") {
+    if (display.textContent[display.textContent.length - 1] == ",") {
+      enableComma();
+    }
+    display.textContent = display.textContent.slice(
+      0,
+      display.textContent.length - 1
+    );
+    if (display.textContent.length === 0) {
+      display.textContent = "0";
+      enableComma();
+    }
+  }
+}
 
-const clearButton = document.getElementById("clearButton");
+function onEqualInput() {
+  const display = document.querySelector("header");
+  num2 = parseFloat(display.textContent.replace(",", "."));
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "x":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 == 0) {
+        result = "ERROR";
+      } else {
+        result = num1 / num2;
+      }
+      break;
+    default:
+      result = display.textContent;
+      break;
+  }
+  let formattedResult = String(result);
 
-clearButton.addEventListener("click",(event)=> {
+  if (formattedResult.length > 9) {
+    if (result > 999999999) {
+      formattedResult = String(result.toExponential(2));
+    } else {
+      formattedResult = formattedResult.slice(0, 8);
+    }
+  }
 
-    clear()
+  if (num1saved) {
+    display.textContent = num1;
+    num1saved = false;
+  } else {
+    display.textContent = formattedResult.replace(".", ",");
+  }
+  const opButtons = document.querySelectorAll(".op");
+  opButtons.forEach((opButton) => {
+    opButton.classList.remove("active");
+    opButton.classList.remove("active-plus");
+  });
+  hasResult = true;
+  operator = "";
+}
 
-})
+function addNumberInputDOMButtonEventListener() {
+  const buttons = document.querySelectorAll("button.num");
+  console.log("input number");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let clickedNum = event.target.textContent;
+      inputNumber(clickedNum);
+    });
+  });
+}
 
+function addClearInputDOMButtonEventListener() {
+  const clearButton = document.getElementById("clearButton");
+  clearButton.addEventListener("click", (event) => {
+    onClearInput();
+  });
+}
 
-const delButton = document.getElementById("delButton");
+function addDeleteInputDOMButtonEventListener() {
+  const delButton = document.getElementById("delButton");
+  delButton.addEventListener("click", (event) => {
+    onDeleteInput();
+  });
+}
 
-delButton.addEventListener("click",(event)=> {
+function addOperatorInputDOMButtonEventListener() {
+  const operators = document.querySelectorAll("button.op");
+  operators.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let clickedOp = event.target;
+      inputOp(clickedOp);
+    });
+  });
+}
 
-    del()
-
-})
-
-const operators = document.querySelectorAll("button.op");
-
-operators.forEach(button => {
-    button.addEventListener("click",(event)=> {
-
-        let clickedOp = event.target
-
-        inputOp(clickedOp)
-    })
-})
-
-const equalButton = document.getElementById("equalButton")
-
-equalButton.addEventListener("click",(event)=> {
-
-    equal()
-
-})
+function addEqualInputDOMButtonEventListener() {
+  const equalButton = document.getElementById("equalButton");
+  equalButton.addEventListener("click", (event) => {
+    onEqualInput();
+  });
+}
